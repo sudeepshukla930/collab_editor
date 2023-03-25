7323ccdd62655c40b2f6c1701441ba8265511447
Sudeep Shukla <59015510+sudeepshukla930@users.noreply.github.com>
2023-03-25 21:16:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -63,5 +63,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
